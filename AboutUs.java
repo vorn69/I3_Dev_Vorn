214af6aad0423213f8d3f519931bd6d15feb1af3
Nha <hellonha228@gamil.com>
2026-03-23 11:02:03 +0700

    Nha Commit v1

--- AboutUs.java
+++
@@ -1,7 +0,0 @@
-public class AboutUs {
-    public static void display() {
-        System.out.println("\n---------- About Us ----------");
-        System.out.println("Developer Team: Piseth, Phy, Vutha");
-        System.out.println("Version: 2026.1.0");
-    }
-}
